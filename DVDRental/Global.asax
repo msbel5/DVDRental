4cf8dc77029c5c0f22d214ceeee6b5f30c770bec
ms bel <msbel5@gmail.com>
2017-10-29 09:02:50 +0300

    Add project files.

---
+++ DVDRental/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="DVDRental.MvcApplication" Language="C#" %>
---
+++ DVDRental/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(DVDRental.Startup))]
+namespace DVDRental
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
